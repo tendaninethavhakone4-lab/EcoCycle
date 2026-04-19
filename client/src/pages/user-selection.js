a7aeaea5a9594da70aa02fcf2e6522a15135bb38
Kamo03 <kamogeloseboks@gmail.com>
2026-04-19 22:07:14 +0200

    Refactor selectRole function and add navigation
    Refactor selectRole function to navigate to target URL.

--- client/src/pages/user-selection.js
+++ client/src/pages/user-selection.js
@@ -11,3 +11,13 @@ function selectRole(card) {
     // updates the colours of the icons when selected
     lucide.createIcons();
 }
+function selectRole(element){
+    const cards=document.querySelectorAll('.role-card');
+    cards.forEach(card=>card.classList.remove('selected'));
+    element.classList.add('selected');
+
+    const targetURL=element.getAttribute('data-url');
+    if(targetURL) {
+        window.location.href=targetURL;
+    }
+}
