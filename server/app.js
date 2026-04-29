e516324d4c38a4c26afa2e90f0df1f98e2b89e27
Tshepi <tshepangmalinga1@gmail.com>
2026-04-30 01:14:42 +0200

    Server setup

--- server/app.js
+++ server/app.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const cors    = require('cors');
+require('dotenv').config();
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users',        require('./routes/users'));
+app.use('/api/transactions', require('./routes/transactions'));
+app.use('/api/materials',    require('./routes/materials'));
+app.use('/api/locations',    require('./routes/locations'));
+app.use('/api/rewards',      require('./routes/rewards'));
+app.use('/api/admin',        require('./routes/admin'));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'EcoCycle API is running!' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
